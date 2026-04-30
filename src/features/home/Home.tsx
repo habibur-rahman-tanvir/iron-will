ff3a7f5b87481f8fbf6e0ab71879ad732f858e7a
habibur-rahman-tanvir <habibur353637@gmail.com>
2026-04-30 23:54:58 +0600

    feat: add CircularProgressbar component to Home and update dependencies

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,6 +1,11 @@
 import { IoSettingsSharp } from "react-icons/io5";
 import ActionBar from "../../shared/components/ActionBar";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,11 +21,24 @@ const Home = () => {
         </ActionBar>
       </div>
 
-      <div className="flex-1">
-        <p>Content</p>
-        <p>
-          <Link to={"/history"}>History</Link>
-        </p>
+      <div className="flex-1 flex justify-center dark:bg-gray-800 items-center">
+        <div className="w-[75%] max-w-lg">
+          <CircularProgressbarWithChildren
+            maxValue={100}
+            value={87}
+            strokeWidth={3}
+            styles={buildStyles({
+              strokeLinecap: "butt",
+            })}
+          >
+            <div className="flex flex-col items-center gap-3">
+              <p className="text-3xl font-bold dark:text-white">2 days</p>
+              <p className="text-xl text-gray-600 dark:text-gray-300">
+                00:00:00
+              </p>
+            </div>
+          </CircularProgressbarWithChildren>
+        </div>
       </div>
     </div>
   );
